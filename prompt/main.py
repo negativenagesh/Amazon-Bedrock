8e9ea8ed4aa48e4653a9a67004eb01e40d51d2c8
Subrahmanya Gaonkar <148525245+negativenagesh@users.noreply.github.com>
2024-09-19 17:06:34 +0530

    used boto3 first time

---
+++ prompt/main.py
@@ -0,0 +1,17 @@
+import boto3
+import json
+
+prompt = """
+
+  Explain me how I can Make Vidyashilp University academic excellence in the field of cyber security. Because I want to integrate it with data science and artificial intelligence.
+  
+"""
+
+bedrock = boto3.client(service_name="bedrock-runtime")
+
+payload = {
+    "prompt": "[INST]" + prompt + "[/INST]]",
+    "max_gen_lemgth": 512,
+    "temperature": 0.5,
+    "top_p": 0.8,
+}
