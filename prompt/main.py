1063c00f08539e54d972006dedccf1c0fa7d22de
Subrahmanya Gaonkar <148525245+negativenagesh@users.noreply.github.com>
2024-09-19 17:36:10 +0530

    Updated main.py with response, and response_text

--- prompt/main.py
+++ prompt/main.py
@@ -18,3 +18,14 @@ payload = {
 
 body=json.dumps(payload)
 model_id="meta.llama2-70b-chat-v1"
+
+response=bedrock.invoke_model(
+  body=body,
+  modelID=model_id
+  accept="application/json"
+  contentType="application/json"
+)
+
+response_body=json.loads(response.get("body").read())
+response_text=responsebody['generation']
+print(response_text)
